477e9cf1960a91170558d3a267b3da495531fab4
rajan321singh-creator <rajan321singh@gmail.com>
2026-09-22 15:37:24 +0300

    Delete Rajan Portfolio directory

--- Rajan Portfolio/OPEN_PORTFOLIO.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title Rajan Singh - Enterprise Portfolio
-echo ========================================================
-echo   RAJAN SINGH - SMART MANUFACTURING PORTFOLIO
-echo   Opening Executive Portal in default web browser...
-echo ========================================================
-start "" "%~dp0index.html"
-exit
